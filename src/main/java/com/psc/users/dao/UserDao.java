7b4f668f218ed7d672693bf5d7f1414c2bf26c7c
murali333 <murali.n333@gmail.com>
2015-06-03 11:21:45 +0530

    New userdao code

--- src/main/java/com/psc/users/dao/UserDao.java
+++ src/main/java/com/psc/users/dao/UserDao.java
@@ -1,16 +1,25 @@
 package com.psc.users.dao;
+import javax.sql.DataSource;
+
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.psc.users.domain.User;
 
 
-public class UserDao extends ConnectionDao {
+public class UserDao  {
 
 	static final Logger logger = Logger.getLogger(UserDao.class); 
+	@Autowired  
+	DataSource dataSource;  
+	
 
 	public void  insertUser(User user) {
 		String sql = "INSERT INTO users "  
 				+ "(username,password, email, mobile) VALUES (?, ?, ?,?)";  
+		
+		JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);  
 		jdbcTemplate.update(  
 				sql,  
 				new Object[] { user.getUsername(),user.getPassword(),user.getEmail(),user.getMobile() });

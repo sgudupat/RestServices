1f0cb273d479f7ed8d9c25cc47716d6af1412b49
david <onlytodavid@gmail.com>
2015-06-04 12:01:12 +0530

    authentication_accounts.sql

--- sql/authentication_accounts.sql
+++ sql/authentication_accounts.sql
@@ -1,6 +1,6 @@
-
 CREATE TABLE `authentication_accounts` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(45) NOT NULL,
   `password` varchar(45) NOT NULL,
-  `config` varchar(1000) NOT NULL
-  );
+  `config` varchar(100) NOT NULL,
+  PRIMARY KEY (`user_id`,`password`,`username`,`config`)
